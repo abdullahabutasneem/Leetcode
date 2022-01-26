1b3705180e7156a0795ec08d68b6a2098b1086bb
abdullahabutasneem <40908199+abdullahabutasneem@users.noreply.github.com>
2022-01-26 11:33:59 +0600

    Create Two Sum.cpp

---
+++ Easy/Two Sum.cpp
@@ -0,0 +1,19 @@
+vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int>mp;
+        vector<int>ans;
+        int a,b;
+        for(int i=0;i<nums.size();i++){
+            int x = target - nums[i];
+            if(mp.find(x)==mp.end()){
+                mp[nums[i]] = i;
+            }
+            else{
+                a = i;
+                b = mp[x];
+                break;
+            }
+        }
+        
+        ans.push_back(a);
+        ans.push_back(b);
+        return ans;
